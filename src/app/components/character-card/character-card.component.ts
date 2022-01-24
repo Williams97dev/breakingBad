e7f74506731b48f59139fceb0659aa345e383af2
Williams97dev <jesusdenisdeveloper@gmail.com>
2022-01-24 15:53:14 +0100

    3 files modified

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,19 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Character } from 'src/app/interfaces/character.interface';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.css']
+  styleUrls: ['./character-card.component.css'],
 })
 export class CharacterCardComponent implements OnInit {
-
   @Input() miCharacter: Character | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,15 @@
 import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Character } from 'src/app/interfaces/character.interface';
-import { CharactersService } from 'src/app/services/characters.service';
+import { Character } from '../../interfaces/character.interface';
+import { CharactersService } from '../../services/characters.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   arrCharacters: Character[];
   mostrarPaginacion: boolean;
   pagination: any[] = [];
@@ -24,45 +23,47 @@ export class HomeComponent implements OnInit {
   }
 
   async ngOnInit() {
-
-    const all = await this.charactersService.getAll()
+    const all = await this.charactersService.getAll();
     const numPages = Math.ceil(all.length / 10);
     this.pagination = new Array(numPages);
 
     //debo discernir entre ruta activa y no ruta activa, si no ruta activa cargo todos los personajes, si hay ruta activa cargo la busqueda
 
-    this.activatedRoute.params.subscribe(async params => {
+    this.activatedRoute.params.subscribe(async (params) => {
       //console.log('params', params)
       if (params.name) {
-        this.arrCharacters = await this.charactersService.getByName(params.name);
+        this.arrCharacters = await this.charactersService.getByName(
+          params.name
+        );
         this.mostrarPaginacion = false;
       } else if (params.page) {
-        this.arrCharacters = await this.charactersService.getByPage(Number(params.page));
+        this.arrCharacters = await this.charactersService.getByPage(
+          Number(params.page)
+        );
         this.mostrarPaginacion = true;
       } else {
         this.arrCharacters = await this.charactersService.getByPage();
         this.mostrarPaginacion = true;
       }
-    })
+    });
 
     //vamos a ver como recogemos queryParams.
 
-    this.activatedRoute.queryParams.subscribe(async queryParams => {
+    this.activatedRoute.queryParams.subscribe(async (queryParams) => {
       //console.log('queryParams', queryParams);
       if (queryParams.category) {
-        this.arrCharacters = await this.charactersService.getByCategory(queryParams.category)
+        this.arrCharacters = await this.charactersService.getByCategory(
+          queryParams.category
+        );
         this.mostrarPaginacion = false;
       } else {
         this.arrCharacters = await this.charactersService.getByPage();
         this.mostrarPaginacion = true;
       }
-
-    })
-
+    });
   }
 
-/* onClick(pPage){
+  /* onClick(pPage){
      this.arrCharacters = await this.charactersService.getByPage(pPage);
 } */
-
 }
